039cf828754c08829a35e7511e429d7e3c220bd4
game1095 <taweesak.in@gmail.com>
2017-01-14 13:35:32 +0700

    added CRUD in employee

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcomes/rake'
-
-  get 'welcomes/routes'
-
   get 'welcomes/index'
   root 'welcomes#index'
   devise_for :managers
+  resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170113101941) do
+ActiveRecord::Schema.define(version: 20170114043959) do
+
+  create_table "employees", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "department"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "managers", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
