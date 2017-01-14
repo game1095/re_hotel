0e7c041be5b61c0d2d50269d88de50de5cdabc59
game1095 <taweesak.in@gmail.com>
2017-01-14 14:38:32 +0700

    room CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'welcomes#index'
   devise_for :managers
   resources :employees
+  resources :rooms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170114043959) do
+ActiveRecord::Schema.define(version: 20170114065446) do
 
   create_table "employees", force: :cascade do |t|
     t.string   "first_name"
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(version: 20170114043959) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_managers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_managers_on_reset_password_token", unique: true
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string   "room_number"
+    t.string   "room_price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
