f8bf359bef494ad291d82922a8025e84f9831360
game1095 <taweesak.in@gmail.com>
2017-01-15 16:35:27 +0700

    commit to master

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,6 @@
 class Employee < ApplicationRecord
+  # validates :first_name , presence: { message: "Story title is required" }
+  validates :first_name , presence: true
+  validates :last_name  , presence: true
+  validates :department , presence: true
 end
