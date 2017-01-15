e9654366e8d3ee2c293ee4f45a0a7405c75cc8eb
game1095 <taweesak.in@gmail.com>
2017-01-15 16:57:00 +0700

    added errors message

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,7 @@
 class Customer < ApplicationRecord
   belongs_to :room
+
+  validates :first_name ,  presence: true
+  validates :last_name , presence: true
+  validates :tel_no ,presence: true
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,7 @@
 class Room < ApplicationRecord
   has_many :customer
+
+  validates :room_number , presence: true
+  validates :room_price , presence: true , numericality: { only_integer: true }
+  validates :room_des  , presence: true
 end
